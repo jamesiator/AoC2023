33786bce28d099168ee534559324797ae264cfba
James Belnap <james6bcdn@gmail.com>
2023-12-08 23:29:24 -0700

    Add day2 part 1

---
+++ day2/day2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+from input import input
+import sys
+
+RED = 'red'
+GREEN = 'green'
+BLUE = 'blue'
+
+def part1(lines: [str]):
+  quantities = {
+    RED: 12,
+    GREEN: 13,
+    BLUE: 14
+  }
+
+  def game_id_if_possible(line: str):
+    game, reveals = line.split(': ')
+    _, game_id = game.split(' ')
+
+    for reveal in reveals.split('; '):
+      for color_reveal in reveal.split(', '):
+        quantity, color = color_reveal.split(' ')
+        if int(quantity) > quantities[color]:
+          return 0
+        
+    return int(game_id)
+  
+  game_id_sum = 0
+  for line in lines:
+    game_id_sum += game_id_if_possible(line)
+
+  return game_id_sum
+
+def part2(lines: [str]):
+  pass
+
+def main():
+  _, part = sys.argv
+  lines = input.split('\n')
+
+  if (int(part) == 1):
+    print(part1(lines))
+  else:
+    print(part2(lines))
+
+main()
